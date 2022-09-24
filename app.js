c661d7566fa3d2ca0a50e13edd5c9a050a9d147c
AldoVarela <varela.aldo83@gmail.com>
2022-09-24 16:24:16 -0300

    creando html

---
+++ app.js
@@ -0,0 +1,17 @@
+const { application } = require('express');
+const express = require('express');
+const app = express();
+
+const path = require('path');
+
+const publicPath = path.resolve (__dirname, './public');
+
+app.use( express.static (publicPath) );
+
+app.listen(3030, ()=>{
+    console.log('Servidor corriendo en puerto 3030!');
+})
+
+app.get ('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/index.html'));
+})
